8af121862c30ec526c168d36eb0cf947e303d259
Waninkoko <waninkoko@gmail.com>
2009-07-27 16:25:44 +0200

    EHCI Module rev14

---
+++ source/main.c
@@ -0,0 +1,49 @@
+/*   
+	Custom IOS Module (EHCI)
+
+	Copyright (C) 2008 neimod.
+	Copyright (C) 2009 kwiirk.
+	Copyright (C) 2009 Hermes.
+	Copyright (C) 2009 Waninkoko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "mem.h"
+#include "timer.h"
+#include "tinyehci.h"
+#include "types.h"
+
+
+int main(void)
+{
+	s32 ret;
+
+	/* Initialize memory heap */
+	Mem_Init();
+
+	/* Initialize timer subsystem */
+	Timer_Init();
+
+	/* Initialize TinyEhci */
+	ret = EHCI_Init();
+	if (ret < 0)
+		return ret;
+
+	/* Main loop */
+	EHCI_Loop();
+
+	return 0;
+}
